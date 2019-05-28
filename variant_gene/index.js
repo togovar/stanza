7d8fdb0a7e14230b6aba9d9a57470d9aa535c6bb
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-05-28 17:33:09 +0900

    fix errors

--- variant_gene/index.js
+++ variant_gene/index.js
@@ -1,35 +1,37 @@
 Stanza(function (stanza, params) {
-  let url = (params.api ? params.api : "").concat("/variant_gene?tgv_id=" + params.tgv_id);
+  if (params.api && params.ep) {
+    let url = (params.api ? params.api : "").concat("/variant_gene?tgv_id=" + params.tgv_id);
 
-  if (params.ep) {
-    url = url.concat("&ep=" + encodeURIComponent(params.ep))
-  }
-
-  if (params.assembly) {
-    url = url.concat("&assembly=" + encodeURIComponent(params.assembly))
-  }
-
-  fetch(url, {method: "GET", headers: {"Accept": "application/json"}}).then(function (response) {
-    if (response.ok) {
-      return response.json();
-    }
-  }).then(function (json) {
-    let bindings = stanza.unwrapValueFromBinding(json);
-    let binding = bindings[0];
-
-    if (binding) {
-      binding.symbol = Array.from(new Set(stanza.grouping(bindings, "symbol").filter(v => v)));
-      binding.synonym = Array.from(new Set(stanza.grouping(bindings, "synonym").filter(v => v)));
+    if (params.ep) {
+      url = url.concat("&ep=" + encodeURIComponent(params.ep))
     }
 
-    stanza.render({
-      template: "stanza.html",
-      parameters: {
-        binding: binding
+    if (params.assembly) {
+      url = url.concat("&assembly=" + encodeURIComponent(params.assembly))
+    }
+
+    fetch(url, {method: "GET", headers: {"Accept": "application/json"}}).then(function (response) {
+      if (response.ok) {
+        return response.json();
       }
+    }).then(function (json) {
+      let bindings = stanza.unwrapValueFromBinding(json);
+      let binding = bindings[0];
+
+      if (binding) {
+        binding.symbol = Array.from(new Set(stanza.grouping(bindings, "symbol").filter(v => v)));
+        binding.synonym = Array.from(new Set(stanza.grouping(bindings, "synonym").filter(v => v)));
+      }
+
+      stanza.render({
+        template: "stanza.html",
+        parameters: {
+          binding: binding
+        }
+      });
+    }).catch(function (e) {
+      stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
+      throw e;
     });
-  }).catch(function (e) {
-    stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
-    throw e;
-  });
+  }
 });
--- variant_summary/index.js
+++ variant_summary/index.js
@@ -1,20 +1,24 @@
 Stanza(function (stanza, params) {
-  stanza.handlebars.registerHelper("print_position", function (binding) {
-    if (!binding) { return }
+  stanza.handlebars.registerHelper("print_position", function (v) {
+    if (!v) {
+      return
+    }
 
-    let [chr, assembly] = binding.reference.replace(/.*\//, "").split("#");
-    let position = binding.stop ? binding.start + ":" + binding.stop : binding.start;
+    let [chr, assembly] = v.reference.replace(/.*\//, "").split("#");
+    let position = v.stop ? v.start + ":" + v.stop : v.start;
 
     return "<span class='chromosome'>" + chr + "</span>"
-        .concat(":<span class='position'>" + position + "</span>")
-        .concat(" <span class='assembly'>(" + assembly + ")</span>");
+      .concat(":<span class='position'>" + position + "</span>")
+      .concat(" <span class='assembly'>(" + assembly + ")</span>");
   });
 
-  stanza.handlebars.registerHelper("print_allele", function (binding) {
-    if (!binding) { return }
+  stanza.handlebars.registerHelper("print_allele", function (v) {
+    if (!v) {
+      return
+    }
 
-    let ref = binding.ref || "";
-    let alt = binding.alt || "";
+    let ref = v.ref || "";
+    let alt = v.alt || "";
 
     if (ref.length === 0) {
       ref = "-"
@@ -26,32 +30,34 @@ Stanza(function (stanza, params) {
     return ref + " / " + alt;
   });
 
-  let url = (params.api ? params.api : "").concat("/variant_summary?tgv_id=" + params.tgv_id);
+  if (params.api && params.ep) {
+    let url = (params.api ? params.api : "").concat("/variant_summary?tgv_id=" + params.tgv_id);
 
-  if (params.ep) {
-    url = url.concat("&ep=" + encodeURIComponent(params.ep))
-  }
-
-  fetch(url, {method: "GET", headers: {"Accept": "application/json"}}).then(function (response) {
-    if (response.ok) {
-      return response.json();
-    }
-  }).then(function (json) {
-    let bindings = stanza.unwrapValueFromBinding(json);
-    let binding = bindings[0];
-
-    if (binding) {
-      binding.hgvs = Array.from(new Set(stanza.grouping(bindings, "hgvs").filter(v => v)));
+    if (params.ep) {
+      url = url.concat("&ep=" + encodeURIComponent(params.ep))
     }
 
-    stanza.render({
-      template: "stanza.html",
-      parameters: {
-        binding: binding
+    fetch(url, {method: "GET", headers: {"Accept": "application/json"}}).then(function (response) {
+      if (response.ok) {
+        return response.json();
       }
+    }).then(function (json) {
+      let bindings = stanza.unwrapValueFromBinding(json);
+      let binding = bindings[0];
+
+      if (binding) {
+        binding.hgvs = Array.from(new Set(stanza.grouping(bindings, "hgvs").filter(v => v)));
+      }
+
+      stanza.render({
+        template: "stanza.html",
+        parameters: {
+          binding: binding
+        }
+      });
+    }).catch(function (e) {
+      stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
+      throw e;
     });
-  }).catch(function (e) {
-    stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
-    throw e;
-  });
+  }
 });
