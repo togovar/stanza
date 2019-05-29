e15f8ab205edfb603be95ec00fa7967f7ce72f02
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-05-29 15:47:24 +0900

    update sparqlist

--- variant_frequency/index.js
+++ variant_frequency/index.js
@@ -1,52 +1,61 @@
-Stanza(function(stanza, params) {
-  stanza.handlebars.registerHelper("locale_string", (str) => {
-    if (str) {
-      return parseInt(str).toLocaleString();
-    } else {
-      return '';
-    }
-  });
-  stanza.handlebars.registerHelper("format_float", (val) => {
-    var v = parseFloat(val);
-    if (v === 0)
-      return '0.0';
-    else if (v === 1)
-      return '1.0';
-    else if (v < 0.001)
-      return v.toExponential(3);
-    else
-      return Math.round(v * Math.pow(10, 3)) / Math.pow(10, 3);
-  });
-  stanza.handlebars.registerHelper("filter_status", (bool) => {
-    if (bool) {
-      return '<span class="green">PASS</span>';
-    } else {
-      return '<span class="red">N/A</span>';
-    }
-  });
+const POPULATION_LABEL = {
+  "JGA-NGS": "Japanese",
+  "JGA-SNP": "Japanese",
+  "3.5k JPN": "Japanese",
+  "HGVD": "Japanese",
+  "ExAC": "Total"
+};
 
-  var results = [params.url1, params.url2].map((url) => {
-    return new Promise((resolve, reject) => {
-      fetch(url, { method: 'GET', headers: { Accept: 'application/json' } }).then((response) => {
-        resolve(response.json());
-      }).catch((error) => {
-        reject('There has been a problem with fetch operation: ' + error.message);
-      });
-    });
-  });
+const ORDER_WEIGHT = {
+  "JGA-NGS": 10,
+  "JGA-SNP": 20,
+  "3.5k JPN": 30,
+  "HGVD": 40,
+  "ExAC": 50,
+  "African/African American": 1,
+  "American": 2,
+  "East Asian": 3,
+  "Finnish": 4,
+  "Non-Finnish European": 5,
+  "Other": 6,
+  "South Asian": 7
+};
 
-  Promise.all(results).then((values) => {
-    stanza.render({
-      template: "stanza.html",
-      parameters: {
-        data: stanza.unwrapValueFromBinding(values[0]),
-        exac_pop: stanza.unwrapValueFromBinding(values[1])
+Stanza(function (stanza, params) {
+  if (params.api) {
+    let url = params.api.concat("/variant_frequency?tgv_id=", params.tgv_id);
+
+    if (params.ep) {
+      url = url.concat("&ep=", encodeURIComponent(params.ep))
+    }
+
+    fetch(url, {method: "GET", headers: {"Accept": "application/json"}}).then(function (response) {
+      if (response.ok) {
+        return response.json();
       }
+    }).then(function (json) {
+      let bindings = stanza.unwrapValueFromBinding(json);
+
+      bindings.forEach(function (binding) {
+        let x = binding.source.split(':');
+        binding.dataset = x[0];
+        binding.population = x[1] || POPULATION_LABEL[x[0]] || "-";
+        binding.order = (ORDER_WEIGHT[x[0]] || 0) + (ORDER_WEIGHT[x[1]] || 0);
+      });
+
+      bindings.sort(function (a, b) {
+        return a.order - b.order;
+      });
+
+      stanza.render({
+        template: "stanza.html",
+        parameters: {
+          bindings: bindings
+        }
+      });
+    }).catch(function (e) {
+      stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
+      throw e;
     });
-    if (!(window.ShadyDOM)) {
-      stanza.select('main').innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">';
-    }
-  }).catch((msg) => { 
-    console.error(msg)
-  });
+  }
 });
