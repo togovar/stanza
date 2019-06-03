0fe2a4076da5caa9204ee40bbc98737a939a1595
平原麗夏 <hiraharareika@cpe-172-101-165-54.rochester.res.rr.com>
2019-06-03 18:20:20 +0900

    pick commit 355bc08 "add style to gene, other alternative alleles and summary" and resolve conflict

--- variant_other_alternative_alleles/index.js
+++ variant_other_alternative_alleles/index.js
@@ -1,102 +1,412 @@
 const DATASETS = [
-  "jga_ngs",
-  "jga_snp",
-  "tommo",
-  "hgvd",
-  "exac"
+	"jga_ngs",
+	"jga_snp",
+	"tommo",
+	"hgvd",
+	"exac"
 ];
 
 const FREQUENCY_TEMPLATE = {
-  source: null,
-  num_alleles: null,
-  num_ref_alleles: null,
-  num_alt_alleles: null,
-  frequency: null,
-  filter: null
+	source: null,
+	num_alleles: null,
+	num_ref_alleles: null,
+	num_alt_alleles: null,
+	frequency: null,
+	filter: null
 };
 
+let consequence_map = {
+	"id": "consequence",
+	"label": "Consequence",
+	"type": "array",
+	"items": [{
+			"id": "SO_0001566",
+			"label": "Regulatory region variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001567",
+			"label": "Stop retained variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001574",
+			"label": "Splice acceptor variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001575",
+			"label": "Splice donor variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001578",
+			"label": "Stop lost",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001580",
+			"label": "Coding sequence variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001583",
+			"label": "Missense variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001587",
+			"label": "Stop gained",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001589",
+			"label": "Frameshift variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001619",
+			"label": "Non coding transcript variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001620",
+			"label": "Mature miRNA variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001621",
+			"label": "NMD transcript variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001623",
+			"label": "5 prime UTR variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001624",
+			"label": "3 prime UTR variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001626",
+			"label": "Incomplete terminal codon variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001627",
+			"label": "Intron variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001628",
+			"label": "Intergenic variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001630",
+			"label": "Splice region variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001631",
+			"label": "Upstream gene variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001632",
+			"label": "Downstream gene variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001782",
+			"label": "TF binding site variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001792",
+			"label": "Non coding transcript exon variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001818",
+			"label": "Protein altering variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001819",
+			"label": "Synonymous variant",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001821",
+			"label": "Inframe insertion",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001822",
+			"label": "Inframe deletion",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001889",
+			"label": "Transcript amplification",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001891",
+			"label": "Regulatory region amplification",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001892",
+			"label": "TFBS amplification",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001893",
+			"label": "Transcript ablation",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001894",
+			"label": "Regulatory region ablation",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001895",
+			"label": "TFBS ablation",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001906",
+			"label": "Feature truncation",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0001907",
+			"label": "Feature elongation",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0002012",
+			"label": "Start lost",
+			"type": "boolean",
+			"default": "1"
+		},
+		{
+			"id": "SO_0002019",
+			"label": "Start retained variant",
+			"type": "boolean",
+			"default": "1"
+		}
+	]
+}
+
 Stanza(function (stanza, params) {
-  stanza.handlebars.registerHelper("print_allele", function (v) {
-    if (!v) {
-      return
-    }
+	stanza.handlebars.registerHelper("print_allele", function (v) {
+		if (!v) {
+			return
+		}
 
-    let ref = v.reference || "";
-    let alt = v.alternative || "";
+		let ref = v.reference || "";
+		let alt = v.alternative || "";
 
-    if (ref.length === 0) {
-      ref = "-"
-    }
-    if (alt.length === 0) {
-      alt = "-"
-    }
+		if (ref.length === 0) {
+			ref = "-"
+		}
+		if (alt.length === 0) {
+			alt = "-"
+		}
 
-    return ref + " / " + alt;
-  });
+		return ref + " / " + alt;
+	});
 
-  stanza.handlebars.registerHelper("so_label", function (accession) {
-    return {
-      "SO_0001483": "SNV",
-      "SO_0000667": "insertion",
-      "SO_0000159": "deletion",
-      "SO_1000032": "indel",
-      "SO_1000002": "substitution"
-    }[accession] || "Unknown";
-  });
+	stanza.handlebars.registerHelper("mapConsequence", function (consequence) {
+		let variant = ''
+		consequence_map.items.forEach(item => {
+			if (item.id === consequence) {
+				variant = item.label
+			}
+		})
+		return variant
+	});
 
-  stanza.handlebars.registerHelper("badge", function (count) {
-    return parseInt(count) > 1 ? '' + (count - 1) + '+' : '';
-  });
+	stanza.handlebars.registerHelper("so_label", function (accession) {
+		return {
+			"SO_0001483": "SNV",
+			"SO_0000667": "insertion",
+			"SO_0000159": "deletion",
+			"SO_1000032": "indel",
+			"SO_1000002": "substitution"
+		} [accession] || "Unknown";
+	});
 
-  stanza.query({
-    endpoint: params.ep ? params.ep : "/sparql",
-    template: "fetch_position.rq",
-    parameters: params
-  }).then(function (data) {
-    let v = stanza.unwrapValueFromBinding(data)[0];
+	stanza.handlebars.registerHelper("getSift", function (sift) {
+		let class_name = sift >= .05 ? 'T' : 'D';
+		return `<span class="variant-function" data-function="${class_name}">${sift}</span>`
+	});
 
-    if (!v) {
-      return
-    }
+	stanza.handlebars.registerHelper("getPolyphen", function (polyphen) {
+		let class_name = ''
+		switch (true) {
+			case polyphen > .908:
+				class_name = 'PROBD';
+				break;
+			case polyphen > .446:
+				class_name = 'POSSD';
+				break;
+			case polyphen >= 0:
+				class_name = 'B';
+				break;
+			default:
+				class_name = 'U';
+				break;
+		}
+		return `<span class="variant-function" data-function="${class_name}">${polyphen}</span>`
+	});
 
-    let url = (params.api ? params.api : "").concat("/search?stat=0&quality=0&term=" + v.chromosome + ":" + v.position);
+	stanza.handlebars.registerHelper("getSignificance", function (significance) {
+		if (significance !== undefined && significance.condition !== undefined) {
+			return `<span class="clinical-significance" data-sign="${significance.interpretations[0]}">${significance.condition}</span>`
+		}
+	});
 
-    fetch(url, {method: "GET", headers: {"Accept": "application/json"}}).then(function (response) {
-      if (response.ok) {
-        return response.json();
-      }
-    }).then(function (json) {
-      let data = json.data ? json.data.filter(v => v.id !== params.tgv_id) : [];
+	stanza.handlebars.registerHelper("getFrequency", function (data) {
+		let frequencyValue = ''
+		let dataSource = ''
+		if (data) {
+			dataSource = data.source
+			switch (true) {
+				case data.num_alt_alleles == 1:
+					frequencyValue = 'singleton';
+					break;
+				case data.frequency >= .5:
+					frequencyValue = '≥0.5';
+					break;
+				case data.frequency > .05:
+					frequencyValue = '<0.5';
+					break;
+				case data.frequency > .01:
+					frequencyValue = '<0.05';
+					break;
+				case data.frequency > .001:
+					frequencyValue = '<0.01';
+					break;
+				case data.frequency > .0001:
+					frequencyValue = '<0.001';
+					break;
+				case data.frequency > 0:
+					frequencyValue = '<0.0001';
+					break;
+				default:
+					frequencyValue = 'monomorphic';
+					break;
+			}
+		} else {
+			frequencyValue = 'na'
+		}
+		return `<div class="dataset" data-dataset="${dataSource}" data-frequency="${frequencyValue}"></div>`
+	});
 
-      data.forEach(function (row) {
-        row.frequencies = DATASETS.map(function (elem) {
-          let obj;
+	stanza.handlebars.registerHelper("badge", function (count) {
+		let badge = parseInt(count) > 1 ? '' + (count - 1) + '+' : ''
+		if (badge !== '') {
+			return `<span class="badge">${badge}</span>`;
+		}
+	});
 
-          if (row.frequencies) {
-            obj = row.frequencies.find(x => x.source === elem)
-          }
+	if (params.api && params.ep) {
+		stanza.query({
+			endpoint: params.ep,
+			template: "fetch_position.rq",
+			parameters: params
+		}).then(function (data) {
+			let v = stanza.unwrapValueFromBinding(data)[0];
 
-          if (!obj) {
-            obj = JSON.parse(JSON.stringify(FREQUENCY_TEMPLATE));
-            obj.source = elem;
-          }
+			if (!v) {
+				return
+			}
 
-          return obj;
-        });
-      });
+			let url = params.api.concat("?stat=0&quality=0&start_only&term=" + v.chromosome + ":" + v.position);
 
-      stanza.render({
-        template: "stanza.html",
-        parameters: {
-          data: data
-        }
-      });
-    }).catch(function (e) {
-      stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
-      throw e;
-    });
-  }).catch(function (e) {
-    stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
-    throw e;
-  });
+			fetch(url, {
+				method: "GET",
+				headers: {
+					"Accept": "application/json"
+				}
+			}).then(function (response) {
+				if (response.ok) {
+					return response.json();
+				}
+			}).then(function (json) {
+				let data = json.data ? json.data.filter(v => v.id !== params.tgv_id) : [];
+
+				data.forEach(function (row) {
+					row.frequencies = DATASETS.map(function (elem) {
+						let obj;
+
+						if (row.frequencies) {
+							obj = row.frequencies.find(x => x.source === elem)
+						}
+
+						if (!obj) {
+							obj = JSON.parse(JSON.stringify(FREQUENCY_TEMPLATE));
+							obj.source = elem;
+						}
+
+						return obj;
+					});
+				});
+
+				stanza.render({
+					template: "stanza.html",
+					parameters: {
+						data: data
+					}
+				});
+			}).catch(function (e) {
+				stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
+				throw e;
+			});
+		}).catch(function (e) {
+			stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
+			throw e;
+		});
+	}
 });
--- variant_summary/index.js
+++ variant_summary/index.js
@@ -1,61 +1,76 @@
 Stanza(function (stanza, params) {
-  stanza.handlebars.registerHelper("print_position", function (v) {
-    if (!v) {
-      return
-    }
+	stanza.handlebars.registerHelper("print_position", function (v) {
+		if (!v) {
+			return
+		}
 
-    let [chr, assembly] = v.reference.replace(/.*\//, "").split("#");
-    let position = v.stop ? v.start + ":" + v.stop : v.start;
+		let [chr, assembly] = v.reference.replace(/.*\//, "").split("#");
+		let position = v.stop ? v.start + ":" + v.stop : v.start;
 
-    return "<span class='chromosome'>" + chr + "</span>"
-      .concat(":<span class='position'>" + position + "</span>")
-      .concat(" <span class='assembly'>(" + assembly + ")</span>");
-  });
+		return "<span class='chromosome'>" + chr + "</span>"
+			.concat(":<span class='position'>" + position + "</span>")
+			.concat(" <span class='assembly'>(" + assembly + ")</span>");
+	});
 
-  stanza.handlebars.registerHelper("print_allele", function (v) {
-    if (!v) {
-      return
-    }
+	stanza.handlebars.registerHelper("print_allele", function (v) {
+		if (!v) {
+			return
+		}
 
-    let ref = v.ref || "";
-    let alt = v.alt || "";
+		let ref = v.ref || "";
+		let alt = v.alt || "";
 
-    if (ref.length === 0) {
-      ref = "-"
-    }
-    if (alt.length === 0) {
-      alt = "-"
-    }
+		if (ref.length === 0) {
+			ref = "-"
+		}
+		if (alt.length === 0) {
+			alt = "-"
+		}
+		let ref_length = ref.length
+		let alt_length = alt.length
+		if (ref.length >= 4) {
+			ref = ref.slice(0, 4) + '...'
+		}
+		if (alt.length >= 4) {
+			alt = alt.slice(0, 4) + '...'
+		}
 
-    return ref + " / " + alt;
-  });
+		return `<span class='ref' data-sum='${ref_length}'>${ref}</span><span class='arrow'></span><span class='alt' data-sum='${alt_length}'>${alt}</span>`;
+	});
 
-  let url = (params.api ? params.api : "").concat("/variant_summary?tgv_id=" + params.tgv_id);
+	if (params.api) {
+		let url = params.api.concat("/variant_summary?tgv_id=" + params.tgv_id);
 
-  if (params.ep) {
-    url = url.concat("&ep=" + encodeURIComponent(params.ep))
-  }
+		if (params.ep) {
+			url = url.concat("&ep=" + encodeURIComponent(params.ep))
+		}
 
-  fetch(url, {method: "GET", headers: {"Accept": "application/json"}}).then(function (response) {
-    if (response.ok) {
-      return response.json();
-    }
-  }).then(function (json) {
-    let bindings = stanza.unwrapValueFromBinding(json);
-    let binding = bindings[0];
+		fetch(url, {
+			method: "GET",
+			headers: {
+				"Accept": "application/json"
+			}
+		}).then(function (response) {
+			if (response.ok) {
+				return response.json();
+			}
+		}).then(function (json) {
+			let bindings = stanza.unwrapValueFromBinding(json);
+			let binding = bindings[0];
 
-    if (binding) {
-      binding.hgvs = Array.from(new Set(stanza.grouping(bindings, "hgvs").filter(v => v)));
-    }
+			if (binding) {
+				binding.hgvs = Array.from(new Set(stanza.grouping(bindings, "hgvs").filter(v => v)));
+			}
 
-    stanza.render({
-      template: "stanza.html",
-      parameters: {
-        binding: binding
-      }
-    });
-  }).catch(function (e) {
-    stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
-    throw e;
-  });
+			stanza.render({
+				template: "stanza.html",
+				parameters: {
+					binding: binding
+				}
+			});
+		}).catch(function (e) {
+			stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
+			throw e;
+		});
+	}
 });
