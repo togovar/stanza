60354ff5d80b86aefd5e4173924c78ec7d102a04
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-06-21 17:06:52 +0900

    display MeSH column as list and refactor

--- variant_publication/index.js
+++ variant_publication/index.js
@@ -19,34 +19,55 @@ Stanza(function (stanza, params) {
       return
     }
 
-    let url = (params.api ? params.api : "").concat("/variant_publication?rs=" + rs.uri.replace(params.prefix, ""));
+    let sparqlist = (params.api ? params.api : "/sparqlist/api").concat("/variant_publication?rs=" + rs.uri.replace(params.prefix, ""));
 
     // if (params.ep) {
-    //   url = url.concat("&ep=" + encodeURIComponent(params.ep))
+    //   sparqlist = sparqlist.concat("&ep=" + encodeURIComponent(params.ep))
     // }
 
     stanza.render({
       template: "stanza.html"
     });
 
-    $.ajax({
-      type: "GET",
-      url: url,
-      dataType: "json",
-      success: function (json) {
-        let headers = "";
-        $.each(json.columns, function (i, val) {
-          headers += "<th data-label = " + val + ">" + val + "</th>";
-        });
-
-        $(stanza.select("#target")).html('<table id="displayTable"><thead><tr>' + headers + '</tr></thead></table>');
-
-        json.searching = false;
-        json.dom = "ilrtfp";
-        json.order = [[2, "desc"]];
-
-        $(stanza.select("#displayTable")).dataTable(json);
+    fetch(sparqlist, {
+      method: "GET",
+      headers: {
+        "Accept": "application/json"
       }
+    }).then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    }).then(function (json) {
+      if (!json) {
+        return ''
+      }
+
+      $(stanza.select("#container")).html('<table id="dataTable"><thead><tr>' + json.columns.map(x => "<th>" + x + "</th>").join("") + '</tr></thead></table>');
+
+      $(stanza.select("#dataTable")).dataTable({
+        columns: json.columns,
+        data: json.data,
+        searching: false,
+        dom: "ilrtfp",
+        order: [[2, "desc"]],
+        columnDefs: [
+          {
+            targets: 4,
+            orderable: false,
+            render: function (data, type, row) {
+              if (type === "display" && Array.isArray(data)) {
+                return "<ul>" + data.map(x => "<li>" + x + "</li>").join("") + "</ul>";
+              } else {
+                return data;
+              }
+            }
+          }
+        ]
+      });
+    }).catch(function (e) {
+      stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
+      throw e;
     });
   }).catch(function (e) {
     stanza.root.querySelector("main").innerHTML = "<p>" + e.message + "</p>";
