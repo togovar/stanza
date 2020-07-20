e219fba8068d56990942c2709e078ca211a806c5
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-07-20 10:55:10 +0900

    fix level indicator

--- variant_other_overlapping_variants/index.js
+++ variant_other_overlapping_variants/index.js
@@ -1,31 +1,23 @@
-const DATASETS = [
-  "jga_ngs",
-  "jga_snp",
-  "tommo_4.7kjpn",
-  "gem_j_wga",
-  "hgvd",
-  "exac"
-];
-
-const FREQUENCY_TEMPLATE = {
-  source: null,
-  num_alleles: null,
-  num_ref_alleles: null,
-  num_alt_alleles: null,
-  frequency: null,
-  filter: null
+const DATASETS = {
+  "gem_j_wga": 1,
+  "jga_ngs": 2,
+  "jga_snp": 3,
+  "tommo_4.7kjpn": 4,
+  "hgvd": 5,
+  "exac": 6
 };
 
 let consequence_map = {
   "id": "consequence",
   "label": "Consequence",
   "type": "array",
-  "items": [{
-    "id": "SO_0001566",
-    "label": "Regulatory region variant",
-    "type": "boolean",
-    "default": "1"
-  },
+  "items": [
+    {
+      "id": "SO_0001566",
+      "label": "Regulatory region variant",
+      "type": "boolean",
+      "default": "1"
+    },
     {
       "id": "SO_0001567",
       "label": "Stop retained variant",
@@ -250,15 +242,16 @@ Stanza(function (stanza, params) {
     });
   }
 
-  stanza.handlebars.registerHelper("print_allele", function (variation) {
-    if (!variation) {
+  stanza.handlebars.registerHelper("print_allele", function (datum) {
+    if (!datum) {
       return
     }
 
-    let ref = variation.reference || "", alt = variation.alternative || "";
+    let ref = datum.reference || ""
+    let alt = datum.alternative || "";
 
-    let ref_length = ref.length;
-    let alt_length = alt.length;
+    const ref_length = ref.length;
+    const alt_length = alt.length;
 
     if (ref.length > 4) {
       ref = ref.slice(0, 4) + "..."
@@ -267,7 +260,11 @@ Stanza(function (stanza, params) {
       alt = alt.slice(0, 4) + "..."
     }
 
-    return `<div class="ref-alt"><span class='ref' data-sum='${ref_length}'>${ref}</span><span class='arrow'></span><span class='alt' data-sum='${alt_length}'>${alt}</span></div>`;
+    return '<div class="ref-alt">' +
+      `<span class="ref" data-sum="${ref_length}">${ref}</span>` +
+      '<span class="arrow"></span>' +
+      `<span class="alt" data-sum="${alt_length}">${alt}</span>` +
+      `</div>`;
   });
 
   stanza.handlebars.registerHelper("mapConsequence", function (consequence) {
@@ -330,46 +327,33 @@ Stanza(function (stanza, params) {
     }
   });
 
-  stanza.handlebars.registerHelper("getFrequency", function (data) {
-    let frequencyValue = "";
-    let dataSource = "";
+  stanza.handlebars.registerHelper("getFrequency", function (frequency) {
+    const allele_count = parseInt(frequency.allele && frequency.allele.count);
+    const f = parseFloat(frequency.allele && frequency.allele.frequency);
 
-    const num_alt_alleles = parseInt(data.num_alt_alleles);
-    const frequency = parseFloat(data.frequency);
+    let level = 'na'
 
-    if (isNaN(data)) {
-      frequencyValue = "na"
+    if (allele_count === 0) {
+      level = 'monomorphic'
+    } else if (allele_count === 1) {
+      level = 'singleton'
     } else {
-      dataSource = data.source;
-      switch (true) {
-        case num_alt_alleles === 1:
-          frequencyValue = "singleton";
-          break;
-        case frequency >= .5:
-          frequencyValue = "≥0.5";
-          break;
-        case frequency > .05:
-          frequencyValue = "<0.5";
-          break;
-        case frequency > .01:
-          frequencyValue = "<0.05";
-          break;
-        case frequency > .001:
-          frequencyValue = "<0.01";
-          break;
-        case frequency > .0001:
-          frequencyValue = "<0.001";
-          break;
-        case frequency > 0:
-          frequencyValue = "<0.0001";
-          break;
-        default:
-          frequencyValue = "monomorphic";
-          break;
+      if (f < 0.0001) {
+        level = '<0.0001'
+      } else if (0.0001 <= f && f < 0.001) {
+        level = '<0.001'
+      } else if (0.001 <= f && f < 0.01) {
+        level = '<0.01'
+      } else if (0.01 <= f && f < 0.05) {
+        level = '<0.05'
+      } else if (0.05 <= f && f < 0.5) {
+        level = '<0.5'
+      } else if (0.5 <= f && f) {
+        level = '≥0.5'
       }
     }
 
-    return `<div class="dataset" data-dataset="${dataSource}" data-frequency="${frequencyValue}"></div>`
+    return `<div class="dataset" data-dataset="${frequency.source}" data-frequency="${level}"></div>`
   });
 
   stanza.handlebars.registerHelper("badge", function (count) {
@@ -402,19 +386,24 @@ Stanza(function (stanza, params) {
     let data = json.data ? json.data.filter(x => x.id !== params.tgv_id) : [];
 
     data.forEach(function (row) {
-      row.frequencies = DATASETS.map(function (elem) {
-        let obj;
+      if (!row.frequencies) {
+        row.frequencies = [];
+      }
 
-        if (row.frequencies) {
-          obj = row.frequencies.find(x => x.source === elem)
+      Object.keys(DATASETS).forEach(function (source) {
+        if (!row.frequencies.find(x => x.source === source)) {
+          row.frequencies.push({source: source});
         }
+      });
 
-        if (!obj) {
-          obj = JSON.parse(JSON.stringify(FREQUENCY_TEMPLATE));
-          obj.source = elem;
+      row.frequencies.sort((a, b) => {
+        if (DATASETS[a.source] < DATASETS[b.source]) {
+          return -1;
         }
-
-        return obj;
+        if (DATASETS[a.source] > DATASETS[b.source]) {
+          return 1;
+        }
+        return 0;
       });
     });
 
