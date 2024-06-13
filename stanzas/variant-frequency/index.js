e9ad34cc035c0610fc750350590a0e9db8d9751b
Yukiko Noda <yukiko8318@gmail.com>
2024-06-13 19:04:40 +0900

    small fix

--- stanzas/variant-frequency/index.js
+++ stanzas/variant-frequency/index.js
@@ -10,9 +10,8 @@ export default class VariantSummary extends Stanza {
     // database icon
     this.importWebFontCSS(new URL("./assets/fontello.css", import.meta.url));
 
-    const { assembly, tgv_id } = this.params;
-    const assemblyNum = assembly.match(/\d+/)
-    const dataURL = `https://stg-grch${assemblyNum}.togovar.org/search?quality=0&term=${tgv_id}&expand_dataset`;
+    const { "data-url": urlBase, assembly, tgv_id } = this.params;
+    const dataURL = `${urlBase}/search?quality=0&term=${tgv_id}&expand_dataset`;
     let resultObject = [];
     let currentLayer1;
 
