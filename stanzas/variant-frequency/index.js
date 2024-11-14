40dddd7777cb755e95bad7b4f9549f71118150e1
Yukiko Noda <yukiko8318@gmail.com>
2024-11-14 13:13:48 +0900

    fix variant frequency

--- stanzas/variant-frequency/index.js
+++ stanzas/variant-frequency/index.js
@@ -1,5 +1,5 @@
 import Stanza from "togostanza/stanza";
-
+import { hierarchy } from 'd3-hierarchy';
 import { DATASETS } from "@/lib/constants";
 import { frequency } from "@/lib/display";
 
@@ -14,7 +14,40 @@ export default class VariantSummary extends Stanza {
     const dataURL = `${urlBase}/search?quality=0&term=${tgv_id}&expand_dataset`;
     let resultObject = [];
     let currentLayer1;
+    let currentLayer2;
     let hasHemizygote = false;
+    let uniqueIdCounter = 0;
+
+    function addIdsToDataNodes(dataNodes, currentDepth = 0) {
+      return dataNodes.map((node) => {
+        // 各ノードに一意のIDを設定
+        const newNode = {
+          ...node,
+          id: `${uniqueIdCounter++}`,
+          depth: currentDepth
+        };
+
+        // 子ノードがある場合は再帰的に処理
+        if (newNode.children && newNode.children.length > 0) {
+          newNode.children = addIdsToDataNodes(newNode.children, currentDepth + 1);
+        }
+        return newNode;
+      });
+    }
+
+    function prepareData() {
+      const data = DATASETS
+      const dataWithIds = addIdsToDataNodes(data);
+
+      const hierarchyData = hierarchy({
+        id: '-1',
+        label: 'root',
+        value: '',
+        children: dataWithIds,
+      });
+
+      return hierarchyData;
+    }
 
     try {
       // dataURL に GET リクエストを送信
@@ -33,13 +66,153 @@ export default class VariantSummary extends Stanza {
       // レスポンスを JSON 形式でパース
       const responseDatasets = await response.json();
       const frequenciesDatasets = responseDatasets.data[0]?.frequencies
-      const datasets = Object.values(DATASETS);
+      const datasets = Object.values(prepareData().data.children);
+      console.log("frequenciesDatasets:", frequenciesDatasets);
+      frequenciesDatasets.forEach((data) => { console.log(data.source) });
+      // frequenciesDatasets.forEach((data) => { console.log(data.source) });
+      // const frequenciesDatasets = [
+      //   {
+      //     "ac": "21",
+      //     "af": 0.00023799999326001853,
+      //     "an": "88,412",
+      //     "filter": [
+      //       "PASS"
+      //     ],
+      //     "quality": 4889.330078125,
+      //     "source": "tommo",
+      //     "dataset": "ToMMo 54KJPN",
+      //     "label": "Japanese",
+      //     "aac": null,
+      //     "arc": null,
+      //     "rrc": null,
+      //     "frequency": "2.380e-4",
+      //     "count": 21,
+      //     "level": "<0.001",
+      //     "class_name": "layer-none"
+      //   },
+      //   {
+      //     "ac": "1",
+      //     "af": 0.000040643801185069606,
+      //     "an": "24,604",
+      //     "filter": [
+      //       "PASS"
+      //     ],
+      //     "source": "gnomad_genomes.afr",
+      //     "dataset": "gnomAD Genomes",
+      //     "label": "African/African American",
+      //     "aac": null,
+      //     "arc": null,
+      //     "rrc": null,
+      //     "frequency": "4.064e-5",
+      //     "count": 1,
+      //     "level": "singleton",
+      //     "class_name": "layer1-nonchild"
+      //   },
+      //   {
+      //     "ac": "6",
+      //     "af": 0.0000651239970466122,
+      //     "an": "92,132",
+      //     "filter": [
+      //       "PASS"
+      //     ],
+      //     "source": "gnomad_genomes",
+      //     "dataset": "gnomAD Genomes",
+      //     "label": "Total",
+      //     "aac": null,
+      //     "arc": null,
+      //     "rrc": null,
+      //     "frequency": "6.512e-5",
+      //     "count": 6,
+      //     "level": "<0.0001",
+      //     "class_name": "layer-total"
+      //   },
+      //   {
+      //     "ac": "0",
+      //     "af": 0,
+      //     "an": "506",
+      //     "filter": [
+      //       "PASS"
+      //     ],
+      //     "source": "gnomad_genomes.ami",
+      //     "dataset": "gnomAD Genomes",
+      //     "label": "Amish",
+      //     "aac": null,
+      //     "arc": null,
+      //     "rrc": null,
+      //     "frequency": "0.0",
+      //     "count": 0,
+      //     "level": "monomorphic",
+      //     "class_name": "layer1-nonchild"
+      //   },
+      //   {
+      //     "ac": "0",
+      //     "af": 0,
+      //     "an": "2,954",
+      //     "filter": [
+      //       "NotHighConfidenceRegion"
+      //     ],
+      //     "quality": 125.5,
+      //     "source": "gem_j_wga",
+      //     "dataset": "GEM-J WGA",
+      //     "label": "Japanese",
+      //     "aac": null,
+      //     "arc": null,
+      //     "rrc": null,
+      //     "frequency": "0.0",
+      //     "count": 0,
+      //     "level": "monomorphic",
+      //     "class_name": "layer-none"
+      //   },
+      //   {
+      //     "aac": "1",
+      //     "ac": "2",
+      //     "af": 0.05000000074505806,
+      //     "an": "40",
+      //     "aoc": 0,
+      //     "arc": "0",
+      //     "filter": [
+      //       "PASS"
+      //     ],
+      //     "hac": 0,
+      //     "hoc": 0,
+      //     "ooc": 0,
+      //     "roc": 0,
+      //     "rrc": "19",
+      //     "source": "jga_wgs",
+      //     "dataset": "JGA-WGS",
+      //     "label": "Total",
+      //     "frequency": "0.050",
+      //     "count": 2,
+      //     "level": "<0.5",
+      //     "class_name": "total-no-children"
+      //   },
+      //   {
+      //     "aac": 0,
+      //     "ac": 2,
+      //     "af": 0.0480000004172325,
+      //     "an": 42,
+      //     "aoc": 0,
+      //     "arc": 2,
+      //     "filter": [
+      //       "PASS"
+      //     ],
+      //     "hac": 0,
+      //     "hoc": 0,
+      //     "hrc": 0,
+      //     "ooc": 0,
+      //     "quality": 2385.11010742188,
+      //     "roc": 0,
+      //     "rrc": 19,
+      //     "source": "jga_wgs.jgad000670"
+      //   }
+      // ]
+      console.log("datasets", datasets);
 
       /** Finds the top-level parent of a given node ID in a nested data structure.
       * @param {Array<Object>} data - The nested data structure to search.
       * @param {string} id - The ID of the node to find the top-level parent for.
       * @returns {Object|null} The top-level parent node or null if not found. */
-      const findTopParent = (data, id) => {
+      const findTopParent = (data, targetId) => {
         // 内部で再帰的に探索する関数
         function recursiveSearch(nodes, targetId, parent = null) {
           for (const node of nodes) {
@@ -56,7 +229,7 @@ export default class VariantSummary extends Stanza {
           }
           return null;
         }
-        return recursiveSearch(data, id);
+        return recursiveSearch(data, targetId);
       }
 
       /** Finds the direct parent of a given node ID in a nested data structure.
@@ -111,11 +284,8 @@ export default class VariantSummary extends Stanza {
         // 一致するデータを探す
         const frequencyData = frequenciesDatasets?.find(x => x.source === datum.value);
         if (frequencyData) {
-          const ac = parseInt(frequencyData.ac);
-          const freq = parseFloat(frequencyData.af);
-
-          // 数値をロケール形式の文字列に変換する関数
-          const localeString = (v) => v !== undefined ? parseInt(v).toLocaleString() : null;
+          // 深さ
+          frequencyData.depth = datum.depth;
 
           // バインディングにデータセット情報を追加
           if (datum.value === 'tommo') {
@@ -126,21 +296,21 @@ export default class VariantSummary extends Stanza {
               case "GRCh38":
                 frequencyData.dataset = "ToMMo 54KJPN";
                 break;
-              default:
-                frequencyData.dataset = "ToMMo";
             }
           } else {
             frequencyData.dataset = findTopParent(datasets, datum.id).label
           }
 
-          if (["gem_j_wga", "jga_ngs", "tommo", "hgvd"].includes(datum.value)) {
+          if (["gem_j_wga", "jga_wes", "tommo", "hgvd"].includes(datum.value)) {
             frequencyData.label = "Japanese";
-          } else if (["jga_snp", "ncbn", "gnomad_genomes", "gnomad_exomes"].includes(datum.value)) {
+          } else if (["jga_wgs", "jga_snp", "ncbn", "gnomad_genomes", "gnomad_exomes"].includes(datum.value)) {
             frequencyData.label = "Total";
           } else {
             frequencyData.label = datum.label;
           }
 
+          // 数値をロケール形式の文字列に変換する関数
+          const localeString = (v) => v !== undefined ? parseInt(v).toLocaleString() : null;
           // Alt
           frequencyData.ac = localeString(frequencyData.ac);
           // Total
@@ -157,6 +327,8 @@ export default class VariantSummary extends Stanza {
           }
 
           // frequencyの情報をバインディングに追加
+          const ac = parseInt(frequencyData.ac);
+          const freq = parseFloat(frequencyData.af);
           Object.assign(frequencyData, frequency(ac, freq));
 
           // 開閉を行うtoggleを作成するため、クラスを設定
@@ -167,7 +339,8 @@ export default class VariantSummary extends Stanza {
           if (
             ![
               'gem_j_wga',
-              'jga_ngs',
+              'jga_wgs',
+              'jga_wes',
               'jga_snp',
               'tommo',
               'ncbn',
@@ -199,28 +372,36 @@ export default class VariantSummary extends Stanza {
 
           // JGA-SNPの場合 見出しのdataがないため追加
           if (frequencyData.dataset === 'JGA-SNP') {
+            // 現在のレイヤーが前のラベルと異なる場合に処理を実行
             if (currentLayer1 !== frequencyData.label) {
-              // ノードの親の数を取得
-              const parentCount = countParents(datasets, datum.id);
-              // 親の数が2である場合に限定して処理を実行
-              if (parentCount === 2 && currentLayer1 !== findParent(datasets, datum.id).label) {
+              // 2階層目であり、前のレイヤーと異なる親ラベルの場合のみ処理
+              if (datum.depth === 2 && currentLayer1 !== findParent(datasets, datum.id).label) {
+                // 結果オブジェクトに階層データを追加
                 let data = {
                   dataset: frequencyData.dataset,
                   label: findParent(datasets, datum.id).label,
                   class_name: 'sub-layer',
                   source: `${frequencyData.dataset}-title`
                 };
-                resultObject = [...resultObject, data];
-                currentLayer1 = findParent(datasets, datum.id).label;
+                resultObject = [...resultObject, data]; // 新たなデータを追加
+                currentLayer1 = findParent(datasets, datum.id).label; // 現在のレイヤーを更新
+              } else if (datum.depth === 3) {
+                console.log("currentLayer2:", currentLayer2);
+                currentLayer2 = findParent(datasets, datum.id).label; // 現在のレイヤーを更新
               }
             }
 
-            if (findParent(datasets, datum.id)?.label) {
+            // 親データのラベルが存在する場合、階層情報を frequencyData に追加
+            const topParentLabel = findParent(datasets, datum.id)?.label;
+            if (topParentLabel) {
+              // 親の数が2つの場合は layer1 に設定
               if (countParents(datasets, datum.id) === 2) {
                 frequencyData.layer1 = findParent(datasets, datum.id).label;
               } else {
-                frequencyData.layer1 = findParent(datasets, findParent(datasets, datum.id).id).label;
-                frequencyData.layer2 = findParent(datasets, datum.id).label;
+                // 親の数が1つの場合、2階層目のラベルとして layer1 と layer2 を設定
+                const grandParentLabel = findParent(datasets, findParent(datasets, datum.id).id).label;
+                frequencyData.layer1 = grandParentLabel;
+                frequencyData.layer2 = topParentLabel;
               }
             }
           }
