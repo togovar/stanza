437ad7b02645dd062997f8c9bd2036527c81dcbc
YukikoNoda <YukikoNoda@users.noreply.github.com>
2026-08-18 01:28:32 +0000

    deploy: 017038eafd887a56f44c26af3d8b521e269ed770

--- variant-frequency.js
+++ variant-frequency.js
@@ -9685,10 +9685,12 @@ class VariantFrequency extends Stanza {
                         "hgvd",
                         "bbj1k",
                         "bbj2k",
-                        "jogo",
                     ].includes(datum.value)) {
                         // 日本人単一集団データセット
                         frequencyData.label = "Japanese";
+                    }
+                    else if (datum.value === "jogo") {
+                        frequencyData.label = "Mixed";
                     }
                     else if ([
                         "jga_wgs",
